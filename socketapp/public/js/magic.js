0ffbca96e6b27a20b9c7b014870b92079410ecd5
lesh <lesh@sysphere.org>
2012-08-01 18:23:02 -0400

    rendering done

--- socketapp/public/js/magic.js
+++ socketapp/public/js/magic.js
@@ -1,5 +1,17 @@
 window.WEB_SOCKET_SWF_LOCATION = "swf/WebSocketMain.swf";
 
+
+var settings = {
+    size: 10,
+    spacing: 2,
+    width: 50,  // those could be received from the serverside too...
+    height: 50  //
+}
+
+
+
+
+
 var demo = {count: 0}
 
 demo.send = function(){
@@ -7,15 +19,50 @@ demo.send = function(){
 	demo.socket.emit('ping');
 }
 
+
+function initializePaper() {  
+    var paper = Raphael("canvas", settings.width * (settings.size + settings.spacing), settings.height * (settings.size + settings.spacing))
+    paper.clearX = function () { 
+        paper.clear()
+        paper.rect(0,0,paper.width,paper.height).attr({fill: '#eeeeee'})
+    }
+    console.log(paper)
+    return paper
+}
+
+function getColour(player) {  
+    return 'red'
+}
+
+function renderGame(paper,data) {      
+    paper.clearX();
+    var size = settings.size
+    var spacing = settings.spacing
+
+    _.map(data, function (point) { 
+        console.log(point)
+        paper.rect(
+            (size + spacing) * point[1][0],  // x
+            (size + spacing) * point[1][1],  // y
+            size, size, 2) // width, height, corner roundness
+            
+            .attr({fill: getColour(point[0]), stroke: "none"}) // colour
+    })
+}
+
+
 $(document).ready(function() {
 	
-	//setup game
-	var canvas = oCanvas.create({canvas: "#canvas", background: "#222"});
-	
-	var render_game = function( data ){
-		for( idx in data.map ){
-		}
-	}
+    var paper = initializePaper()
+
+    renderGame(paper,[
+        [1, [3,3],1],
+        [1, [3,4],1],
+        [1, [3,5],1],
+        [1, [2,5],1],
+        [1, [1,5],1]
+    ])
+
 
 	//setup sockets
 	demo.socket = io.connect('http://0.0.0.0:80');
@@ -36,7 +83,7 @@ $(document).ready(function() {
 
 	//player state update
 	demo.socket.on('update_state',function(data){
-		render_game( data );
+		renderGame(paper,data);
 		console.log('state updated');
 	});
 
