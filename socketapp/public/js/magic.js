5ac0150a7f09bcd232cbd0492a9aeea2448b5aa3
Mike Silagadze <mike@tophatmonocle.com>
2012-08-01 17:31:03 -0400

    added game logic and stuff

--- socketapp/public/js/magic.js
+++ socketapp/public/js/magic.js
@@ -8,7 +8,18 @@ demo.send = function(){
 }
 
 $(document).ready(function() {
-	demo.socket = io.connect('http://127.0.0.1:8080');
+	
+	//setup game
+	var canvas = oCanvas.create({canvas: "#canvas", background: "#222"});
+	
+	var render_game = function( data ){
+		for( idx in data.map ){
+		}
+	}
+
+	//setup sockets
+	demo.socket = io.connect('http://0.0.0.0:80');
+	demo.socket_ch1 = io.connect('http://0.0.0.0:80/ch1');
 
 	demo.socket.on('pong', function (data) {
 		console.log('pong '+demo.count);
@@ -18,5 +29,28 @@ $(document).ready(function() {
 	demo.socket.on('hello', function (data) {
 		console.log('hello '+data.hello);
 	});
+
+	demo.socket_ch1.on('hello', function(data){
+		console.log('hello '+data.hello);
+	});
+
+	//player state update
+	demo.socket.on('update_state',function(data){
+		render_game( data );
+		console.log('state updated');
+	});
+
+	//key bindings
+	$(document).keydown(function(e){
+		if(e.keyCode == 37) { //left
+			demo.socket.emit('keypress', {direction:'left'});
+			console.log('left')
+		} else if(e.keyCode == 39) { //right
+			demo.socket.emit('keypress', {direction:'right'});
+			console.log('right');
+		}
+	});
 });
+
+
 
--- socketapp/server.js
+++ socketapp/server.js
@@ -2,11 +2,17 @@ var http = require('http')
   , express = require('express') 
   , io = require('socket.io');
 
+// game_state {
+//   map: [] //array of points [player, [x,y], age]
+//   players: [{ session: session_id, current: [], vector: [], age_cutoff: [] },]
+// }
+// player_move_queue = [{session: session_id, move: 'left'},]
+// generation is a counter for the number of state upates so far
+var game_state = {map: [], players: []};
+var player_move_queue = [];
+var generation = 0;
+
 //setup http server
-//express 2.0.0
-//var app = express.createServer();
-//var server = app;
-//express 3.0.0
 var app = express();
 var server = http.createServer(app);
 
@@ -20,7 +26,7 @@ app.get('/', function(req,res){
 	res.render('index', {title: 'websockets demo'});
 });
 
-server.listen(8080)
+server.listen(80,'0.0.0.0')
 
 //setup websocket server
 ss = io.listen(server);
@@ -28,11 +34,69 @@ ss = io.listen(server);
 ss.set('transports', ['websocket','flashsocket','htmlfile','xhr-polling','jsonp-polling']);
 
 ss.sockets.on("connection", function(socket){
+	//dummy hello world stuff
 	socket.emit('hello', { hello: 'world' });
 	socket.on('ping', function(){
 		socket.emit('pong');
 	});
 	console.log('socket connection!');
+
+	//setup player
+	//   players: [{ session: session_id, current: [], vector: [], age_cutoff: [] },]
+	game_state.players.push( {session: socket.id, current:[100,100], vector: [0,1], age_cutoff: [10]} )
+	game_state.map.push( {session: socket.id, coord: [100,100], generation:i 0} )
+	
+	//setup key events
+	socket.on('keypress', function(data){
+		//add player move to queue
+		console.log('keypress '+data.direction);
+		player_move_queue.push([socket.id, data.direction]);
+	});	
 });
+
+ss.of('/ch1').on('connection', function(socket){
+	socket.emit('hello', {hello:'channel 1'});
+});
+
+//setup state update and broadcase
+var update_state = function(){
+	generation++;
+	console.log('generation: '+generation);
+	
+	//process player moves
+	for( idx in player_mode_queue ){
+		//find player
+		
+		//rotate player vector
+	}
+
+	//clear move queue
+	player_move_queue = [];
+
+	//update game state
+	for( idx in game_state.players ){
+	}
+
+	//kill off snake sections that are past their generation
+	for( idx in game_state.map ){
+		//update generation
+		//if too old, kill it
+	}
+}
+
+var broadcast_state = function(){
+	//for each active session, send the updated state
+	var clients = ss.sockets.clients(); 
+	for( client in clients ) {
+		clients[client].emit('update_state', game_state);
+	}
+}
+
+setInterval(function(){
+	update_state();
+	broadcast_state();
+}, 3000);
+
+
 
 
