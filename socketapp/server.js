a626450a7eb762e35e5fb66121aefa5e723e5293
Mike Silagadze <mike@tophatmonocle.com>
2012-08-01 07:23:26 -0400

    maybe works with dotclound finally?

--- socketapp/server.js
+++ socketapp/server.js
@@ -3,8 +3,12 @@ var http = require('http')
   , io = require('socket.io');
 
 //setup http server
-var app = express();
-var server = http.createServer(app);
+//express 2.0.0
+var app = express.createServer();
+var server = app;
+//express 3.0.0
+//var app = express();
+//var server = http.createServer(app);
 
 app.configure(function(){
 	app.set('views', __dirname + '/views');
@@ -13,7 +17,10 @@ app.configure(function(){
 });
 
 app.get('/', function(req,res){
-	res.render('index', {title: 'websockets demo'});
+//express 3.0.0
+//	res.render('index', {title: 'websockets demo'});
+//experss 2.0.0
+	res.send("<!DOCTYPE html><html lang='en'><head><title>websockets demo</title><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script><script src='js/socket.io.js'></script><script src='js/magic.js'></script></head><body><h1>websockets demo</h1><button type='button' onclick='demo.send()'>Send</button></body></html>");
 });
 
 server.listen(8080)
